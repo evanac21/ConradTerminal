0fc2b33feac8411999ab70ea1a06975d9c882bb1
Evan Carter <32000073+evanac21@users.noreply.github.com>
2019-03-11 00:35:10 -0700

    worked on node

--- node/app.js
+++ node/app.js
@@ -1,1 +1,10 @@
-const rls = require('readline-sync')
+const exp = require('express')
+const app = exp()
+const port = 3000
+var path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+  app.listen(3000)
